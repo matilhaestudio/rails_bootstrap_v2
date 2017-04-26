85be500b67f1fad908bc784fcec385a542a5ba09
Jonas Scherer <jonas@matilhaestud.io>
2017-04-26 11:11:39 -0300

    adding code coverage :wip

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
 source 'https://rubygems.org'
-
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.7.1'
 # Postgres SQL
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,14 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  #Test Coverage
+  require 'simplecov'
+  require 'codacy-coverage'
+
+  SimpleCov.start 'rails'
+  Codacy::Reporter.start
+  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
