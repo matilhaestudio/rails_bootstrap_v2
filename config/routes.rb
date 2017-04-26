1945e844f35500d2f6fb0ee7d22114f94478488c
Jonas Scherer <jonas@matilhaestud.io>
2017-04-26 17:16:10 -0300

    adding translations to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
  	#root "pages#home", as: 'home'
- 	root "devise/sessions#new"
 	devise_for :users
+	
+	devise_scope :user do
+		root "devise/sessions#new"
+  	end
 end
