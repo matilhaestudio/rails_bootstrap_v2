2a20cbdd343d6e39c472fb2b953236cc50d4a198
Jonas Scherer <jonas@matilhaestud.io>
2017-06-02 10:35:46 -0300

    initializing mongo db on app initializer

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module RailsKickstartServer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    Mongoid.load!("config/mongoid.yml")
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
