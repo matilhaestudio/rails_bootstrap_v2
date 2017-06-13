35abd7a7784f89a660a07beea8305ae7222014b7
Jonas Scherer <jonas@matilhaestud.io>
2017-06-13 11:05:30 -0300

    adding sample model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612204555) do
+ActiveRecord::Schema.define(version: 20170613135610) do
+
+  create_table "samples", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone",        limit: 15
+    t.string   "email"
+    t.string   "zipcode",      limit: 9
+    t.string   "street"
+    t.string   "number"
+    t.string   "neighborhood"
+    t.string   "city"
+    t.string   "state"
+    t.string   "complement"
+    t.date     "birthday"
+    t.string   "picture"
+    t.string   "cpf",          limit: 14
+    t.string   "rg",           limit: 12
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
