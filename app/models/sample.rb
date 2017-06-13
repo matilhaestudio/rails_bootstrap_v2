1ab178d2fde8390ebeaafacf1549b90c1feda0e3
Jonas Scherer <jonas@matilhaestud.io>
2017-06-13 12:03:06 -0300

    adding validations

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,2 +1,16 @@
 class Sample < ApplicationRecord
+	validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_uniqueness_of :cpf, case_sensitive: false
+  validates_cpf_format_of :cpf
+  validates :cpf, presence: true
+  validates :zipcode, presence: true, format: { with: /\A[0-9]{5}-[0-9]{3}\z/ }
+  validates :street, presence: true
+  validates :number, presence: true
+  validates :neighborhood, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :phone, presence: true
+  validates :birthday, presence: true, length: { is: 10, message: 'data inválida' }
+  validates :email, presence: true, email: true
 end
