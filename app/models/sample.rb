f9622cc7e8e615550f3fb05fa15f17a1b91ae1c8
Marcelo Barreto <marcelobarretojunior@gmail.com>
2017-06-26 10:32:29 -0300

    validations on samples

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,23 +1,20 @@
 class Sample < ApplicationRecord
-# validates :first_name, presence: true
- #  validates :last_name, presence: true
- #  validates_uniqueness_of :cpf, case_sensitive: false
- #  validates_cpf_format_of :cpf
- #  validates_cpf_format_of :cnpj
- # 	validates_cpf_format_of :website
- #  validates :cpf, presence: true
- #  validates :zipcode, presence: true
- #  validates :street, presence: true
- #  validates :number, presence: true
- #  validates :neighborhood, presence: true
- #  validates :city, presence: true
- #  validates :state, presence: true
- #  validates :phone, presence: true
- #  validates :birthday, presence: true, length: { is: 10, message: 'data inválida' }
- #  validates :email, presence: true, email: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_uniqueness_of :cpf, case_sensitive: false
+  validates :website, url: true
+  validates :cpf, presence: true, cpf: true
+  validates :cnpj, presence: false, cnpj: true
+  validates :zipcode, presence: true
+  validates :street, presence: true
+  validates :number, presence: true
+  validates :neighborhood, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :phone, presence: true
+  validates :birthday, presence: true, length: { is: 10, message: 'data inválida' }
+  validates :email, presence: true, email: true
 
   mount_uploader :file, FileUploader
   mount_uploader :picture, PhotoUploader
-  
-
 end
